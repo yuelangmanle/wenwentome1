d4594ca07794c128e1a91fe34622fc5b8c987165
月亮满了 <yueliangmanle@yueliangmanledeMacBook.local>
2026-03-22 00:33:42 +0800

    test: anchor about screen metadata assertions

--- app/src/testDebug/java/com/wenwentome/reader/AppAboutAndChangelogFlowTest.kt
+++ app/src/testDebug/java/com/wenwentome/reader/AppAboutAndChangelogFlowTest.kt
@@ -36,8 +36,8 @@ class AppAboutAndChangelogFlowTest {
 
         composeTestRule.onNodeWithTag("nav-settings").performClick()
         composeTestRule.onNodeWithText("关于与项目").assertTextEquals("关于与项目")
-        composeTestRule.onNodeWithText("作者：月亮满了").assertTextEquals("作者：月亮满了")
-        composeTestRule.onNodeWithText("版本 ${BuildConfig.VERSION_NAME}")
+        composeTestRule.onNodeWithTag("project-author-text").assertTextEquals("作者：月亮满了")
+        composeTestRule.onNodeWithTag("project-version-text")
             .assertTextEquals("版本 ${BuildConfig.VERSION_NAME}")
         composeTestRule.onNodeWithTag("open-changelog-button").performScrollTo().performClick()
         composeTestRule.waitUntil(timeoutMillis = 5_000) {
